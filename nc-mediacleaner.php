2832f058783506f92a1dac2f0b641d6db78cadf6
Rik Meijer <hello@rikmeijer.nl>
2025-06-11 09:17:41 +0200

    always assume century
    no automatic guessing works flawless without a century

--- nc-mediacleaner.php
+++ nc-mediacleaner.php
@@ -39,29 +39,25 @@ $attempt = new Attempt(new Sabre\DAV\Client([
         ]));
 
 $files_base_path = '/remote.php/dav/files/' . NEXTCLOUD_USER;
-
-$available_year_directories = array_slice($attempt('propfind', $files_base_path . NEXTCLOUD_UPLOAD_PATH, [
-    '{DAV:}displayname',
-    '{DAV:}getcontentlength'
-        ], 1), 1);
-
 $padleft = fn(array $arr) => array_map(fn($val) => str_pad($val, 2, '0', STR_PAD_LEFT), $arr);
 
+$years = join('|', $padleft(range('00', date('y'))));
 $months = join('|', $padleft(range('01', '12')));
 $file_regexes = [
-    '/^(?<century>20)?(?<year>' . join('|', $padleft(range('00', date('y')))) . ')(?<month>' . $months . ')(?<day>[0-2][1-9]|3[0-1])/',
-    '/^(?<century>20)?(?<year>' . join('|', $padleft(range('00', date('y')))) . ')(?<sep>[-_:])(?<month>' . $months . ')(?P=sep)(?<day>[0-2][1-9]|3[0-1])/',
-    '/^(?<day>[0-2][1-9]|3[0-1])(?<century>20)(?<month>' . $months . ')(?<year>' . join('|', $padleft(range('00', date('y')))) . ')/',
+    '(^|\D)(?<year>(19|20)(' . $years . '))(?<month>' . $months . ')(?<day>[0-2][1-9]|3[0-1])',
+    '(^|\D)(?<year>(19|20)(' . $years . '))(?<sep>[-_:])(?<month>' . $months . ')(?P=sep)(?<day>[0-2][1-9]|3[0-1])',
+    '(^|\D)(?<day>[0-2][1-9]|3[0-1])(?<month>' . $months . ')(?<year>(19|20)(' . $years . '))'
 ];
 
-foreach ($available_year_directories as $year_directory_id => $available_year_directory) {
-    $year = basename($year_directory_id);
-    $available_month_directories = array_slice($attempt('propfind', $year_directory_id, [
-        '{DAV:}displayname',
-        '{DAV:}getcontentlength'
-            ], 1), 1);
+$propfind = fn(string $path) => array_slice($attempt('propfind', $path, [
+            '{DAV:}displayname',
+            '{DAV:}getcontentlength'
+                        ], 1), 1);
 
-    foreach ($available_month_directories as $month_directory_id => $available_month_directory) {
+foreach ($propfind($files_base_path . NEXTCLOUD_UPLOAD_PATH) as $year_directory_id => $available_year_directory) {
+    $year = basename($year_directory_id);
+
+    foreach ($propfind($year_directory_id) as $month_directory_id => $available_month_directory) {
         $month = basename($month_directory_id);
         $actual_location = $year . '/' . $month;
         IO::write('Actual locaton: ' . $actual_location);
@@ -70,15 +66,18 @@ foreach ($available_year_directories as $year_directory_id => $available_year_di
             '{DAV:}getcontentlength',
             '{http://nextcloud.org/ns}creation_time'
                         ], 1), 1);
+
+        IO::write(count($available_media_files) . ' files');
         foreach ($available_media_files as $file_id => $available_media_file) {
             $expected_location = date('Y/m', $available_media_file['{http://nextcloud.org/ns}creation_time']);
 
             foreach ($file_regexes as $file_regex) {
-                if (preg_match($file_regex, basename($file_id), $matches) !== 1) {
+                if (preg_match('/' . $file_regex . '/', basename($file_id), $match, PREG_UNMATCHED_AS_NULL) !== 1) {
                     continue;
                 }
 
-                $expected_location = (empty($matches['century']) ? '20' : $matches['century']) . $matches['year'] . '/' . $matches['month'];
+                $expected_location = $match['year'] . '/' . $match['month'];
+                break;
             }
 
             if ($actual_location !== $expected_location) {
